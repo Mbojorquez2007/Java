e874ed181a7064e0c1d24267162a3b6acf52adc0
Michelle <mbojorquez4@cnm.edu>
2017-08-07 06:03:29 -0600

    added java

--- java.js
+++ java.js
@@ -1,29 +1,29 @@
 function a clever man doesnt need it…() {
-	document.getElementById("box").innerHTML = "<strong>BUZZ OFF!</strong>";
-	document.getElementById("box").style.backgroundColor = "red";
+	document.getElementById("box").innerHTML = "<strong>Help Me!</strong>";
+	document.getElementById("box").style.backgroundColor = "blue";
 }
 
-function beRelieved() {
-	document.getElementById("box").innerHTML = "So glad to finally be rid of you!";
-	document.getElementById("box").style.backgroundColor = "orange";
+function a fool won’t take it…() {
+	document.getElementById("box").innerHTML = "Do unto others as you would have other do unto you";
+	document.getElementById("box").style.backgroundColor = "purple";
 }
 
-function congratulateMacUsers() {
-	var macUsers = ["Ryan", "Steven", "Alex", "Jack"];
+function konnichiwa cleverUsers() {
+	var cleverUsers = ["Jack", "Jill", "Old lady", "Old man"];
 	var userList = "";
 
-	macUsers.sort();
-	macUsers.map(function(user) {
+	cleverUsers.sort();
+	cleverUsers.map(function(user) {
 		console.log("current user list: " + userList);
 		userList = userList + "<li>" + user + "</li>";
 	});
-	document.getElementById("macUsers").innerHTML = userList;
+	document.getElementById("cleverUsers").innerHTML = userList;
 }
 
-function saySomething() {
+function Always say what you mean and mean what you say() {
 	document.getElementById("box").innerHTML = document.getElementById("boxContent").value;
 }
 
-function startAnnoyance() {
+functionAlways say what you mean and mean what you say() {
 	alert("Red alert!");
 }
