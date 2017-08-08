20e42c77f7756602d5715d01d66cb06c5b30c55c
Michelle <mbojorquez4@cnm.edu>
2017-08-07 23:54:06 -0600

    addaed code

---
+++ script.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Javascript</title>
+		<script/>
+	</head>
+
+	<body>
+
+		<p> id="welcome"</p>
+		<form>
+			What is your name:<input type="text" id="name"/><br/>
+			<input type="button" value="submit" onclick="writeName();"/>
+		</form>
+		<h1></h1>
+
+		<p></p>
+	</body>
+</html>
