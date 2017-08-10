f33ef1013f489c52bb1958fa598829baa68eb27e
Michelle <mbojorquez4@cnm.edu>
2017-08-09 18:38:31 -0600

    added code

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
 	function welcomeButton() {
-		alert("Welcome");
+		alert("Yay!");
 
 	}
+});
+
+function changeColor() {
+	document.getElementById("colorbox").style.backgroundColor = "black";
+	document.getElementById("colorbox").style.color = "white";
+}
+function changeBack() {
+	document.getElementById("colorbox").style.borderRadius = "50%";
+}
+
+//* this is the code to run the AJAX bacon button
+$(document).ready(function() {
+	$(("#getJqueryBacon").click(function() {
+		$.ajax({
+			method: "GET",
+			url: "https://baconipsum.com/api/?type=meat-and-filler"
+		}).done(function(reply) {
+			$("#jqueryBacon").html(reply);
+			console.log("you have juery flavored bacon!");
+
+		});
+
+	});
+
 });
--- script.php
+++ script.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<title>Javascript</title>
 		<script src="script.js"></script>
-		<link rel="stylesheet" href="css.css">
+		<link rel="stylesheet" href="style.css">
 	</head>
 
 	<body>
@@ -13,8 +13,9 @@
 			What is your name:<input type="text" name="type your name here">
 
 
-			<button type="button" onclick="alert('Welcome')">Click me</button>
+			<button type="button" onclick="alert('Yay!')">Click me</button>
 		</form>
+		<div id="colorbox" onmouseenter="changeColor();" onmouseover="changeColor();" onmouseout="changeBack();"<h4>I love you!</h4></div>
 		<h1></h1>
 
 		<p></p>
