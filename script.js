6b150fa06ce6f316c92cf5a16101970a7bb3bb44
Michelle <mbojorquez4@cnm.edu>
2017-08-09 19:53:17 -0600

    added jquery

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-	function welcomeButton() {
-		alert("Yay!");
-
-	}
-});
+// $(document).ready(function() {
+// 	function welcomeButton() {
+// 		alert("Yay!");
+//
+// 	}
+// });
 
 function changeColor() {
 	document.getElementById("colorbox").style.backgroundColor = "black";
@@ -13,19 +13,18 @@ function changeBack() {
 	document.getElementById("colorbox").style.borderRadius = "50%";
 }
 
-//* this is the code to run the AJAX bacon button
+// this is the code to run the AJAX bacon button
 $(document).ready(function() {
-	$(("#baconButton").click(function() {
+	$("#baconButton").click(function() {
 		$.ajax({
 			method: "GET",
 			url: "https://baconipsum.com/api/?type=meat-and-filler&paras=5&format=text"
 		}).done(function(reply) {
-			$("ipsumOutput").html(reply);
 			$("#baconOutput").html(reply);
 			console.log("bacon bacon bacon!");
 
 		});
 
-	}));
+	});
 
 });
--- script.php
+++ script.php
@@ -3,8 +3,11 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Javascript</title>
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 		<script src="script.js"></script>
 		<link rel="stylesheet" href="style.css">
+
 	</head>
 
 	<body>
@@ -18,7 +21,7 @@
 		<div id="colorbox" onmouseenter="changeColor();" onmouseover="changeColor();" onmouseout="changeBack();"<h4>I love you!</h4></div>
 		<h3>pigOut</h3>
 		<button id="baconButton">piggy</button>
-		<div>class=container id="baconOutput">
+		<div id="baconOutput">
 			<!--IPSUM goes here-->
 		</div>
 		<h1></h1>
